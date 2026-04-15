14c65d72d50e66b1808bd8c70c41364a1423bf73
Katie <kalufukatie@gmail.com>
2026-04-14 22:31:29 -0400

    Fixed incomplete comment.

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
     //Setting a variable (computer value) equal to the randomly chosen option.
     const computerValue = computerThrows[choiceNumber];
 
-    //Setting the computer.
+    //Setting the computer value to the final choice.
     setComputerChoice(computerValue);
 
     //Calling getFinal to deterime if they win/lose/tie.
